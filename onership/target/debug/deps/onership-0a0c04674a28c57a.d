5627bec9c44e115ac91d76d42211763a67d4a46b
pedro1798 <peter584aa@naver.com>
2023-11-11 08:27:17 +0000

    Ownership

---
+++ onership/target/debug/deps/onership-0a0c04674a28c57a.d
@@ -0,0 +1,5 @@
+/root/projects/onership/target/debug/deps/onership-0a0c04674a28c57a: src/main.rs
+
+/root/projects/onership/target/debug/deps/onership-0a0c04674a28c57a.d: src/main.rs
+
+src/main.rs:
---
+++ onership/target/debug/onership.d
@@ -0,0 +1,1 @@
+/root/projects/onership/target/debug/onership: /root/projects/onership/src/main.rs
