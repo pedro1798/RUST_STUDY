74e6b9cabc788d02620a6282d4deb07bd26b5939
pedro1798 <peter584aa@naver.com>
2023-11-12 09:59:58 +0000

    rectangle3

---
+++ 5.Using_Structs_to_Structure_Related_Data/derived_trait/target/debug/deps/derived_trait-68d011588ae9e119.d
@@ -0,0 +1,5 @@
+/root/projects/5.Using_Structs_to_Structure_Related_Data/derived_trait/target/debug/deps/derived_trait-68d011588ae9e119: src/main.rs
+
+/root/projects/5.Using_Structs_to_Structure_Related_Data/derived_trait/target/debug/deps/derived_trait-68d011588ae9e119.d: src/main.rs
+
+src/main.rs:
---
+++ 5.Using_Structs_to_Structure_Related_Data/derived_trait/target/debug/derived_trait.d
@@ -0,0 +1,1 @@
+/root/projects/5.Using_Structs_to_Structure_Related_Data/derived_trait/target/debug/derived_trait: /root/projects/5.Using_Structs_to_Structure_Related_Data/derived_trait/src/main.rs
