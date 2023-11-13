7f373fbf426f49307e3b0bcaa98b2d185946739d
pedro1798 <peter584aa@naver.com>
2023-11-13 10:52:00 +0000

    예전에 공부했던 내용 까먹고 보고 있었는데 지금보니 몰랐던 내용이라 그냥 commit함.

--- 6.Enums_and_Pattern_Matching/shadowing/src/main.rs
+++ 6.Enums_and_Pattern_Matching/shadowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let x = 10; // shadowing
+    {
+        let x = x * 2;
+        println!("The value of in the inner scope is: {x}");
+    }  // 이 스코프가 끝나면 안쪽의 섀도잉은 끝나서 x는 다시 10으로 돌아온다.
+    println!("The value of x is: {x}");
+    // let을 사용하면, 값을 변형하면서 변형이 완료된 후에는 불변으로 유지할 수 있다.
 }
