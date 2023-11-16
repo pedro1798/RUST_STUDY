0bc9a38c89826b38acc610d1e64a368ffb235898
pedro1798 <peter584aa@naver.com>
2023-11-16 15:05:03 +0000

    dependency에 크레이트 추가하면 모듈 사용 시 pub mod로 선언 안 해줘도 됨..?

--- 7.Packages_Crates_Modules/the_main/src/main.rs
+++ 7.Packages_Crates_Modules/the_main/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     );
     the_lib::sibling1::hello();
     the_lib::sibling2::hello();
+    // the_lib을 디펜던시에 추가하지 않았다면 
+    // pub mod the_lib 으로해당 모듈을 외부에서 접근 가능하게 선언해야됨.
 }
 
-// sibling modules일지라도 다른 파일에 구현되면 pub 없인 접근할 수 없음.
+// sibling modules일지라도 다른 파일에 독립적으로 구현되면 pub 없인 접근할 수 없음.
